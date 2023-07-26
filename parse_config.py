6ddfc8061671a98b82f2de86edf59d486b52fc4f
salvoch <manuel@salvoch.com>
2023-07-26 00:52:29 -0600

    Complete parse_config, simplified the type checking

--- parse_config.py
+++ parse_config.py
@@ -3,20 +3,15 @@ Parse the config.yaml file
 '''
 import yaml
 from dataclasses import dataclass
-from typing import TypeAlias
+from typing import TypeAlias, NewType
 
-#Defined exceptions
-#TODO - make these properly
-class ConfigFileError(Exception):
-    pass
-
-#Defined types
-#TODO - Look into making a proper data structure
-MarkdownFileName:  TypeAlias = str
-CategoryName:      TypeAlias = str
+# Defined types
+MarkdownFileName = NewType('MarkdownFileName', str)
+CategoryName = NewType('CategoryName', str)
 CategoryStructure: TypeAlias = dict[CategoryName, list[MarkdownFileName]]
 
-#Defined data structures
+
+# Defined data structure
 @dataclass
 class ConfigFile:
     '''Parsed config file'''
@@ -24,24 +19,37 @@ class ConfigFile:
     categories: list[CategoryStructure]
     style: str
 
+    def __post_init__(self) -> None:
+        '''Runs after init, validates types match and format'''
+        # NOTE - Cannot check instance of MarkdownFileName in isinstance()
+        if not isinstance(self.index, str):
+            raise TypeError("Config 'index' field"
+                            " should be the name of a markdown file")
+
+        if not isinstance(self.style, str):
+            raise TypeError("Config 'style' field"
+                            " should be a string denoting style option.")
+
+        if not isinstance(self.categories, list):
+            raise TypeError("Categories field is formed"
+                            " incorrectly, should result in list.")
+
+
 def parse_config_file(filename: str = "config.yaml") -> ConfigFile:
     '''Parse the yaml config file for a project'''
-    
+
     with open(filename, 'r') as f:
         yaml_data = yaml.safe_load(f)
 
-    try:
-        config_file = ConfigFile(
-            index = str(yaml_data['index']),
-            categories = yaml_data['categories'], #TODO- type check this
-            style = str(yaml_data['style'])
-        )
-    except KeyError as exc:
-        raise ConfigFileError(exc) #TODO - Raise these correctly
-    except Exception as exc:
-        raise ConfigFileError(exc)
+    # Load config file
+    config_file = ConfigFile(
+        index=MarkdownFileName(yaml_data['index']),
+        categories=yaml_data['categories'],
+        style=str(yaml_data['style'])
+    )
 
     return config_file
+
 
 if __name__ == '__main__':
     a = parse_config_file()
