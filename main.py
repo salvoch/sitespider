cd666206eaa73bb21bd51a3d0d7154a15d14cc3d
salvoch <manuel@salvoch.com>
2023-08-17 00:31:29 -0600

    Process our categories into a list of thier respective objects

--- main.py
+++ main.py
@@ -39,6 +39,55 @@ def main(output_build_directory: str = DEFAULT_OUTPUT_DIRECTORY) -> None:
     config_file = parse_config_file()
     build_dir = make_directories(config_file, output_build_directory)
 
+    # Generate SiteNotes Objects
+    # -- Main Index
+    index_md_path = Path(config_file.index)
+    index_stripper = PandocStripper(index_md_path)
+    html_path = build_dir / "index.html"
+    index: MainIndex = MainIndex(md_path=index_md_path,
+                                 html_path=html_path,
+                                 images=index_stripper.strip_local_image_locations(),
+                                 title=index_stripper.strip_title()
+                                 )
+
+    # -- Category Indices
+    categories: list[CategoryIndex] = []
+    for category_dict in config_file.categories:
+        for category_title in category_dict.keys():
+            html_path = build_dir / category_title / "index.html"
+            categories.append(
+                CategoryIndex(
+                    md_path=None,
+                    html_path=html_path,
+                    images=[],
+                    title=category_title,
+                    category_name=category_title
+                )
+            )
+
+    # -- Site Note Objects
+    site_notes: list[SiteNote] = []
+    for category_dict in config_file.categories:
+        for category_title, notes in category_dict.items():
+            for note in notes:
+                note_path = Path(note)
+                note_stripper = PandocStripper(note_path)
+                html_path = build_dir / category_title / (Path(note).stem+".html")
+                site_notes.append(
+                    SiteNote(
+                        md_path=note_path,
+                        html_path=html_path,
+                        images=note_stripper.strip_local_image_locations(),
+                        title=note_stripper.strip_title(),
+                        category_name=category_title
+                    )
+                )
+
+    # TEST PRINT RM
+    print(f'index = {index}')
+    print(f'categories = {categories}')
+    print(f'site notes = {site_notes}')
+
     # Render html
     renderer = Renderer(config_file, build_dir)
     renderer.render_index()
