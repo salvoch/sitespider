ca9627a960019c0ea71457515823214e9fd649eb
salvoch <manuel@salvoch.com>
2023-08-17 00:30:23 -0600

    define dataclasses for site objects Instead of having a single one, define a class for each category: * Main site index * Category Index * Note post This will allow us to write functions for each type, since the processing will be different for each

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 #!/Users/msalvoch/Personal/sitespider/.venv/bin/python3
 import sys
+from strip_elements import PandocStripper, ImageReference
+from pathlib import Path
+from dataclasses import dataclass
 from parse_config import parse_config_file
 from make_directories import make_directories
 from render_html import Renderer
 DEFAULT_OUTPUT_DIRECTORY = 'build'
+
+
+@dataclass
+class PageObject:
+    md_path: Path | None
+    html_path: Path
+    images: list[ImageReference]
+    title: str
+
+
+@dataclass(kw_only=True)
+class MainIndex(PageObject):
+    pass
+
+
+@dataclass(kw_only=True)
+class CategoryIndex(PageObject):
+    category_name: str
+
+
+@dataclass(kw_only=True)
+class SiteNote(PageObject):
+    category_name: str
 
 
 def main(output_build_directory: str = DEFAULT_OUTPUT_DIRECTORY) -> None:
