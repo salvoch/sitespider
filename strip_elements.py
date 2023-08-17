080d1341a9958077c5f1cab60833b7323d0ec612
salvoch <manuel@salvoch.com>
2023-08-17 00:29:21 -0600

    Mak strip_elements a class again When put in use, it was very obvious quickly that a class would have been better we store the padnoc file, we will not have this be separate

--- strip_elements.py
+++ strip_elements.py
@@ -13,75 +13,74 @@ class ImageReference:
     md_target: str
 
 
-def pandify_file(file_path: Path) -> PandocFileType:
-    '''Given a Path object to a markdown file, return the file converted into a Pandoc object'''
+class PandocStripper:
 
-    # TODO - This is reused from render_html.convert_md_file_to_html, how to avoid?
-    # Check to make sure file exists
-    if not file_path.exists():
-        raise Exception  # TODO - create actual exception
+    def __init__(self, file_path: Path) -> None:
+        '''Given a Path object to a markdown file, return the file converted into a Pandoc object'''
 
-    # Check to make sure .md file was passed
-    if file_path.suffix != ".md":
-        raise Exception  # TODO - create actual exception
+        # TODO - This is reused from render_html.convert_md_file_to_html, how to avoid?
+        # Check to make sure file exists
+        if not file_path.exists():
+            raise Exception  # TODO - create actual exception
 
-    with file_path.open() as file:
-        read_file = file.read()
-        pan_doc = pandoc.read(read_file, format="markdown")
+        # Check to make sure .md file was passed
+        if file_path.suffix != ".md":
+            raise Exception  # TODO - create actual exception
 
-    return pan_doc
+        with file_path.open() as file:
+            read_file = file.read()
+            self.pandoc_file = pandoc.read(read_file, format="markdown")
 
+    def strip_title(self) -> str:
+        '''Return the first H1 header as a string, can safely be assumed this is the title'''
 
-def strip_title(pandoc_file: PandocFileType) -> str:
-    '''Return the first H1 header as a string, can safely be assumed this is the title'''
+        if len(self.pandoc_file) < 2:
+            print("Empty .md file, no title to strip")  # TODO - do this in a logger.warning()
+            return ""
 
-    if len(pandoc_file) < 2:
-        print("Empty .md file, no title to strip")  # TODO - do this in a logger.warning()
+        # Iterate through each element, find a #H1 header
+        for element in self.pandoc_file[1]:
+            if isinstance(element, pandoc.types.Header):
+                if element[0] == 1:
+                    # Remove the preceding "# ", and strip the \n, \r, and \t's.
+                    # Also, preserve any additional # and \ found within the sentence
+                    pan_string = pandoc.write(element, format="markdown")
+                    return "# ".join(pan_string.strip().split('# ')[1:]).replace("\\", "")
+
+        print("No title (aka an H1 (#)) element found in file ")  # TODO - do this in a logger.warning()
+        # TODO - Also, specify which file!
         return ""
 
-    # Iterate through each element, find a #H1 header
-    for element in pandoc_file[1]:
-        if isinstance(element, pandoc.types.Header):
-            if element[0] == 1:
-                # Remove the preceding "# ", and strip the \n, \r, and \t's.
-                # Also, preserve any additional # and \ found within the sentence
-                pan_string = pandoc.write(element, format="markdown")
-                return "# ".join(pan_string.strip().split('# ')[1:]).replace("\\", "")
+    def strip_local_image_locations(self) -> list[ImageReference]:
+        '''Find all local images (that do not contain a url) in a file and return a list of their locations
+        This will be useful when converting old references to the new file structure reference
 
-    print("No title (aka an H1 (#)) element found in file ")  # TODO - do this in a logger.warning()
-    return ""
+        This will not be used for converting images themselves to HTML, pandoc does this. However, this will
+        be used to copy the existing images to their expected location'''
 
+        if len(self.pandoc_file) < 2:
+            print("Empty .md file, no images to strip")  # TODO - do this in a logger.warning()
+            return []
 
-def strip_local_image_locations(pandoc_file: PandocFileType) -> list[ImageReference]:
-    '''Find all local images (that do not contain a url) in a file and return a list of their locations
-    This will be useful when converting old references to the new file structure reference
+        # Find all paragraphs first, images are only inside paragraphs, not standalone
+        # Then, iterate through each element inside each paragraph, looking for images
+        image_list = []
+        for element in self.pandoc_file[1]:
+            if isinstance(element, pandoc.types.Para):
+                for sub_element in element[0]:
+                    if isinstance(sub_element, pandoc.types.Image):
+                        inline_text = sub_element[1][0].__dict__['_args'][0]
+                        md_target = sub_element[2][0]
+                        image_list.append(ImageReference(
+                            inline_text=inline_text,
+                            md_target=md_target))
 
-    This will not be used for converting images themselves to HTML, pandoc does this. However, this will
-    be used to copy the existing images to their expected location'''
-
-    if len(pandoc_file) < 2:
-        print("Empty .md file, no images to strip")  # TODO - do this in a logger.warning()
-        return []
-
-    # Find all paragraphs first, images are only inside paragraphs, not standalone
-    # Then, iterate through each element inside each paragraph, looking for images
-    image_list = []
-    for element in pandoc_file[1]:
-        if isinstance(element, pandoc.types.Para):
-            for sub_element in element[0]:
-                if isinstance(sub_element, pandoc.types.Image):
-                    inline_text = sub_element[1][0].__dict__['_args'][0]
-                    md_target = sub_element[2][0]
-                    image_list.append(ImageReference(
-                        inline_text=inline_text,
-                        md_target=md_target))
-
-    # Remove images from the web, since these will be unused
-    url_check = lambda a: a[0:8].lower() == "https://" or a[0:7].lower() == "http://"
-    return [x for x in image_list if not url_check(x.md_target)]
+        # Remove images from the web, since these will be unused
+        url_check = lambda a: a[0:8].lower() == "https://" or a[0:7].lower() == "http://"
+        return [x for x in image_list if not url_check(x.md_target)]
 
 
 if __name__ == '__main__':
-    pan_obj = pandify_file(Path('test.md'))
-    print(strip_title(pan_obj))
-    print(strip_local_image_locations(pan_obj))
+    pan_obj = PandocStripper(Path('test.md'))
+    print(pan_obj.strip_title())
+    print(pan_obj.strip_local_image_locations())
