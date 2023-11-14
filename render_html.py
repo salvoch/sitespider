f903a74dcc40537e1efe7242330e71e4d8bf8eb6
salvoch <manuel@salvoch.com>
2023-11-13 21:47:20 -0700

    First steps towards render

--- render_html.py
+++ render_html.py
@@ -4,17 +4,39 @@ import pandoc
 from pandoc.types import Pandoc
 from strip_elements import PandocStripper, ImageReference
 
+# TODO -
+    # Have to create two additional page types potentially
+        # - Sub index (the page below the index, this has a list of categories and top 3 posts
+        # - About page (thing on the left hand - can we make it optional?)
+    # We have to pass a list of categories/posts to:
+        # The sub index (the categories, and the latest 3 posts)
+        # Category index (Just the posts in its own category)
+    # Can we pass a list of objects to these?
+    # Complete the three image functions
+        # Locate images
+        # Move images
+        # Update refs to images
+
 
 class PageObject(ABC):
     '''Generic page class'''
 
     def __init__(self, md_path: Path, build_dir: Path) -> None:
+
+        # Exception checking
+        # Check to make sure passed file was an .md file
+        if md_path.suffix != ".md":
+            raise Exception  # TODO - create actual exception
+
+        # Passed data
         self.md_path: Path = md_path
         self.build_dir: Path = build_dir
 
+        # Generated data
         self.title: str = self.generate_title()
         self.html_path: Path = self.generate_html_path()
 
+        # Empty data
         self.images: list[ImageReference] = []
         self.pandoc_page: Pandoc | None = None
 
@@ -63,7 +85,18 @@ class MainIndex(PageObject):
         return self.build_dir / "index.html"
 
     def render(self) -> None:
-        ...
+        # Convert file to html
+        # TODO - should we make this a try?
+        with self.md_path.open() as file:
+            read_file = file.read()
+            self.pandoc_page = pandoc.read(read_file, format="markdown")
+
+            # Get list of images
+            # Move images
+            # Update image locations
+            # Templetize
+
+            pandoc.write(self.pandoc_page, self.html_path, "html")
 
     def __str__(self) -> str:
         return_str = f'''Page Type: MainIndex
@@ -96,6 +129,8 @@ class CategoryIndex(PageObject):
 
     def render(self) -> None:
         ...
+        # Generate sub-list of pages in category
+        # Put in a template
 
     def __str__(self) -> str:
         return_str = f'''Page Type: CategoryIndex
@@ -122,7 +157,19 @@ class SiteNote(PageObject):
         return self.build_dir / self.category_name / (self.md_path.stem+".html")
 
     def render(self) -> None:
-        ...
+        # Convert file to html
+        # TODO - should we make this a try?
+        with self.md_path.open() as file:
+            read_file = file.read()
+            self.pandoc_page = pandoc.read(read_file, format="markdown")
+
+            # Get list of images
+            # Move images
+            # Update image locations
+            # Generate header
+            # Templetize
+
+            pandoc.write(self.pandoc_page, self.html_path, "html")
 
     def __str__(self) -> str:
         return_str = f'''Page Type: SiteNote
